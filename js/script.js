8fd5057e81ddde16148cd273539741385e7f6fd3
André Bez Birolo <andrecentrobrasileiro@outlook.com>
2021-04-08 21:09:11 -0300

    Adicionado BackToTop(ISSUE) e corrigido bugs

--- js/script.js
+++ js/script.js
@@ -82,4 +82,20 @@ document.addEventListener("DOMContentLoaded", function () {
   fixedMenu();
   typeWriter(document.querySelector('h1'));
 
+  function backToTop() {
+    const backToTopButton = document.querySelector('.back-to-top')
+    function toggleVisibility() {
+      (scrollY > 200) ? backToTopButton.classList.add('visible') : backToTopButton.classList.remove('visible')
+    }
+    window.addEventListener('scroll', debounce(() => {
+      toggleVisibility();
+    }), 200);
+    backToTopButton.addEventListener('click', () => {
+      document.body.scrollTop = 0
+      document.documentElement.scrollTop = 0
+    })
+    toggleVisibility();
+  }
+  backToTop();
+
 });
