2e8edc50eda40842c5b9ab79ada87b2f415adbed
André Bez Birolo <andrecentrobrasileiro@outlook.com>
2021-04-27 20:59:42 -0300

    Inserção da mascara do telefone

--- js/script.js
+++ js/script.js
@@ -119,6 +119,36 @@ function lazyLoadImg() {
   }
 }
 
+function inputMaskPhone(input) {
+  function treatment(value, isOnBlur) {
+    value = value.replace(/\D/g, "");
+    value = value.replace(/^(\d{2})(\d)/g, "($1)$2");
+    if (isOnBlur) {
+      value = value.replace(/(\d)(\d{4})$/, "$1-$2");
+    } else {
+      value = value.replace(/(\d)(\d{3})$/, "$1-$2");
+    }
+    return value;
+  }
+  input.onkeypress = function (evt) {
+    var code = window.event ? window.event.keyCode : evt.which;
+    var value = this.value;
+    if (code > 57 || (code < 48 && code != 8)) {
+      return false;
+    } else {
+      this.value = treatment(value, false);
+    }
+  };
+  input.onblur = function () {
+    var value = this.value;
+    if (value.length < 13) {
+      this.value = "";
+    } else {
+      this.value = treatment(this.value, true);
+    }
+  };
+  input.maxLength = 14;
+}
 
 
 document.addEventListener("DOMContentLoaded", function () {
@@ -145,4 +175,6 @@ document.addEventListener("DOMContentLoaded", function () {
   Pageclip.form(form, {
     successTemplate: '<span>Obrigado pelo contato!</span>'
   })
+
+  inputMaskPhone(document.querySelector('#whatsapp'))
 });
