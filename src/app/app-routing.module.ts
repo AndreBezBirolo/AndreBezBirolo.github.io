f79e0e840b54e147a0e9a337da0901076d7bae74
André Bez Birolo <andrecentrobrasileiro@outlook.com>
2021-09-30 16:29:07 -0300

    Criado as rotas e títulos dinâmicos para o site

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,48 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NotFoundComponent } from './errors/not-found/not-found.component';
+import { HomeComponent } from './home/home.component';
+import { TrajectoryComponent } from './trajectory/trajectory.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'home'
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+    data: {
+      title: 'André Bez Birolo - Desenvolvedor Web'
+    }
+  },
+  {
+    path: 'trajectory',
+    component: TrajectoryComponent,
+    data: {
+      title: 'André Bez Birolo - Trajetória'
+    }
+  },
+  {
+    path: 'not-found',
+    component: NotFoundComponent,
+    data: {
+      title: 'André Bez Birolo - Ops..'
+    }
+  },
+  {
+    path: '**',
+    redirectTo: 'not-found'
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot(routes, { useHash: true })
+  ],
+  exports: [
+    RouterModule
+  ]
 })
 export class AppRoutingModule { }
