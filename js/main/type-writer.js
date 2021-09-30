17eeb0edaa1f81c5a11158aa76024cd2ede0f45d
André Bez Birolo <andrecentrobrasileiro@outlook.com>
2021-09-30 15:56:41 -0300

    Arquivos iniciais da refatoração

--- js/main/type-writer.js
+++
@@ -1,7 +0,0 @@
-const typeWriter = function (element) {
-  const textToAnime = element.innerText.split('');
-  element.innerText = '';
-  textToAnime.forEach((letter, i) => {
-    setTimeout(() => element.innerText += letter, 75 * i);
-  });
-}
