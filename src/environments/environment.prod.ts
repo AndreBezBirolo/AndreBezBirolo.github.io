2d9ccb7b44ad1a0af49c69ba747a8ef98ace23ad
André Bez Birolo <andrecentrobrasileiro@outlook.com>
2023-06-03 12:45:03 -0300

    fix: corrigido variaveis de ambiente

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
-  GITHUB_IMAGE_URL: 'https://raw.githubusercontent.com/AndreBezBirolo',
+  GITHUB_BASE_URL: 'https://api.github.com/',
+  GITHUB_IMAGE_URL: 'https://raw.githubusercontent.com/AndreBezBirolo/',
   LINKEDIN_LINK: 'https://www.linkedin.com/in/andre-jbb/',
   YOUTUBE_LINK: 'https://www.youtube.com/channel/UCeRfhBW4-fgxUmO-v9bOt2Q',
   GITHUB_LINK: 'https://github.com/AndreBezBirolo'
